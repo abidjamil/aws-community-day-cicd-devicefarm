70c41ffd18650cee2a3014c0dbfbf7d59729b23c
Abid Jamil <abidjamil222@gmail.com>
2024-02-20 22:57:01 +0500

    Compile SDK version 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.anaario.awscommunityday"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.anaario.awscommunityday"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
